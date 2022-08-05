2998bd3862b96520241c3c8542f8220c37f842b2
sk <samirkabra1161@gmail.com>
2022-08-04 22:18:03 -0400

    Restructured elements, added stop draw featuire

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let mode = 'default';
 let gridsize = 16;
+let memoryMode = 'default';
 
 function changeColor() {
     if(mode == 'default'){
         this.style.background = "black";
+    } else if(mode == 'random') {
+        r = Math.floor(Math.random() * 255);
+        g = Math.floor(Math.random() * 255);
+        b = Math.floor(Math.random() * 255);
+        console.log(r,g,b);
+        this.style.background = `rgb(${r}, ${g}, ${b})`;
+    } else if(mode == 'none') {
+        return
     }
 }
 
@@ -18,6 +27,7 @@ function createGrid(gridsize) {
         newDiv = document.createElement("div");
         newDiv.classList.add("canvas_element");
         newDiv.addEventListener('mouseenter', changeColor)
+        newDiv.addEventListener('contextmenu', drawMode)
         parentDiv.appendChild(newDiv)
     }
 }
@@ -36,4 +46,27 @@ function clearGrid() {
     createGrid(gridsize);
 }
 
+function randomMode(){
+    mode = 'random';
+}
+
+function defaultMode() {
+    mode = 'default';
+}
+
+function drawMode() {
+    if(mode == 'none') {
+        mode = memoryMode;
+    }
+    else {
+        memoryMode = mode;
+        mode = 'none';
+    }
+}
+
 createGrid(gridsize)
+
+/*document.addEventListener('contextmenu' ,() => {
+    
+})
+*/
